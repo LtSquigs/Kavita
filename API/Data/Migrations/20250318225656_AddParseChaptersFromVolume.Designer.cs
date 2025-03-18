4a9da12c26ab3a98d2ad7ffb13852e57d0bdd58f
Ian Kelly <ltsquigs@gmail.com>
2025-03-18 17:57:32 -0500

    readd migration

--- API/Data/Migrations/20250318225656_AddParseChaptersFromVolume.Designer.cs
+++ API/Data/Migrations/20250318225656_AddParseChaptersFromVolume.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250318225656_AddParseChaptersFromVolume")]
+    partial class AddParseChaptersFromVolume
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.2");
